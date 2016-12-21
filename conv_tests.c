86aee884f75e08f150e28db9eaf1447238ec8477
Johan Kämpe <kampe.johan@gmail.com>
2016-12-21 15:43:20 +0100

    Added program to test functions in libconvert

---
+++ conv_tests.c
@@ -0,0 +1,19 @@
+/* 
+Mjukvaruutvecklare Inbyggda System
+Applicerad Yrkesmatematik
+Julprojekt 2016
+
+Johan Kämpe 
+
+
+Code used to test functions in libconvert library
+*/
+
+#include <stdio.h>
+#include "lib/libconvert.h"
+#include "test/tests.h"
+
+int main(){
+	binary_tests();
+	return 0;
+}
