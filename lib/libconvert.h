1d76fa1bffafd852880326362db9bcdc030e5d6e
Johan Kämpe <kampe.johan@gmail.com>
2016-12-21 15:42:55 +0100

    Corrected function names

--- lib/libconvert.h
+++ lib/libconvert.h
@@ -10,6 +10,6 @@
 #define DECIMALTECKEN 10
 
 void vand(char *str);
-char *convert_i_to_binary(int decimal);
-char *convert_f_to_binary(double decimal);
-char *convert_to_binary(double decimal);
+char *convert_to_binary(int decimal);
+char *convert_to_binary_frac(double decimal);
+char *convert_to_binary_TEMPNAMNFIXA(double decimal);
