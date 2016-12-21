2b013e414999f2be2e3a27c1de2f66c6aa8203b0
Johan Kämpe <kampe.johan@gmail.com>
2016-12-21 16:31:24 +0100

    Added test for new function convert_to_base

--- conv_tests.c
+++ conv_tests.c
@@ -14,6 +14,12 @@ Code used to test functions in libconvert library
 #include "test/tests.h"
 
 int main(){
-	binary_tests();
+	//binary_tests(); -- OK
+	char *converted_base_n;
+	 
+	 //Test base 16 -- Shall be 2AE05  -- OK!
+	converted_base_n = convert_to_base(175621, 16);
+	puts(converted_base_n);
+	
 	return 0;
 }
