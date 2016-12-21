8980b34b2e71687b0f3b8ac5be605a8a7f91f0e1
Johan Kämpe <kampe.johan@gmail.com>
2016-12-21 19:36:44 +0100

    Added standard libs, added declarations of new functions

--- lib/libconvert.h
+++ lib/libconvert.h
@@ -7,10 +7,11 @@ Johan Kämpe
 */
 
 #include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-//Antal tecken för i binära textsträngar
+//Antal tecken för textsträngar
 #define N 256
 
 //Antal siffror efter kommatecken, för binära flyttal
@@ -20,7 +21,13 @@ void vand(char *str);
 char *convert_to_binary(int decimal);
 char *convert_to_binary_frac(double decimal);
 char *convert_decimal_to_binary(double decimal);
+
 char *convert_to_base(int decimal, int base);
-char numToChar(int num);
 char *convert_to_base_frac(double dec_frac, int max_digits, int base);
 char *convert_dec_to_base(double dec_frac, int max_digits, int base);
+
+double convert_base_to_dec(char *base_frac, int max_digits, int base);
+
+
+char numToChar(int num);
+int charToNum(char num);
