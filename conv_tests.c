ab68fb305bc141179802f4b5fb7d4fd0a1660377
Johan Kämpe <kampe.johan@gmail.com>
2016-12-26 22:01:29 +0100

    Updated code to test new fractal base n conversion functions

--- conv_tests.c
+++ conv_tests.c
@@ -27,10 +27,19 @@ int main(){
 	
 	//Manual tests
 	char *converted_number;
+	double  converted_number2;
 	puts("MANUAL CONVERSION");
+		while(1){
+			int base = 2;
+			char num[N];
+			printf("Enter number and base: ");
+			scanf("%s %d", &num, &base);
+			converted_number2 = convert_base_to_dec(num, 10, base);
+			printf("%s(%d) = %.6f(10)\n", num, base, converted_number2);
+	}
 	while(1){
-		int base = 2;
 		double dec;
+		int base = 2;
 		printf("Enter decimal number and base: ");
 		scanf("%lf %d", &dec, &base);
 		converted_number = convert_dec_to_base(dec, 10, base);
