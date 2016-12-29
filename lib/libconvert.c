e9a5c9d99fdc9e587b9b304424cb9fbc6ddf8d77
Johan Kämpe <kampe.johan@gmail.com>
2016-12-29 17:58:23 +0100

    Extensive renaming of variables and functions, to camelBack format

--- lib/libconvert.c
+++ lib/libconvert.c
@@ -11,7 +11,7 @@ Johan Kämpe
 /* Vänder på en char-array.
 Används för konvertering från hela decimala heltal till tal 
 med godtycklig bas. */
-void vand(char *str){
+void reverseString(char *str){
 	//lower / upper index
 	int lix, uix = strlen(str)-1;
 	char temp;
@@ -22,60 +22,7 @@ void vand(char *str){
 	}
 }
 
-//Konverterar tal med bas 10 till tal med valfri bas 'base'
-char *convert_to_base(int decimal, int base){
-	char *omvand = malloc(sizeof(char) * N);
-	//oix: omvandling index:
-	int t, oix, num;
-	oix = 0;
-	//Om inkommande tal att omvandla är 0, returneras 0
-	if(!decimal){
-		omvand[oix] = '0';
-		oix++;
-	}
-	else{
-		/* Siffran är resten av delning med talbasen 'base'
-		på det decimala talet. */
-		for(t = decimal; t != 0; t=t/base){
-			num = t % base;
-			omvand[oix] = numToChar(num);
-			oix++;
-		}
-	}
-	omvand[oix] = '\0';
-	//Vänder på tecknen i char-arrayen
-	vand(omvand);
-	return omvand;
-}
-
-//Konverterar decimalt flyttal till tal med basen 'base'
-char *convert_to_base_frac(double dec_frac, int max_digits, int base){
-	char *base_frac = malloc(sizeof(char) * N);
-	//oix: omvandling index:
-	int oix = 0, num;
-	/* Parameter anses vara i form: 0.##### 
-	Därför sätts strängens början till .# 	*/
-	base_frac[oix] = '.';
-	oix++;
-	if(!dec_frac){
-		base_frac[oix] = '0';
-		oix++;
-	}
-	else{
-		for(dec_frac = dec_frac*base; dec_frac != 0.0 && 
-		max_digits > 0; dec_frac=dec_frac*base){
-			num = dec_frac;
-			dec_frac = dec_frac - num ;
-			base_frac[oix] = numToChar(num);
-			oix++;
-			max_digits--;
-		}
-	}
-	base_frac[oix] = '\0';
-	return base_frac;
-}
-
-//Konverterar int till char 0-9: 0-9 11+: A+
+//Konverterar heltalsvärde till enstaka chartecken 0-9: 0-9 11+: A+
 char numToChar(int num){
 	char c;
 	/* ASCII-tecknet '0' har värdet 48, och siffran 0 har 0. 
@@ -94,112 +41,149 @@ char numToChar(int num){
 	return c;
 }
 
-//Konverterar char 0-9: 0-9 : A-Z : 10-36(?)
+//Konverterar chartecken till heltalsvärde 0-9: 0-9 : A-Z : 10-36
 int charToNum(char num){
-	//printf("char->num c: %c", num);
 	int n;
 	if('0' <= num && num <= '9'){
-		//printf(" ( if 0<10 ) ");
 		n = num - '0';
 	}
-	//toupper returnerar versalen av en char
+	//toupper returnerar versalen av ett char-tecken.
 	else if ('A' <= toupper(num) && toupper(num) <= 'Z'){
-		//printf(" ( if ABC ) ");
 		//A = 10, B = 11 osv.
 		n = num - 'A' + 10;
 	}
-	//printf(" int: %d\n", n);
 	return n;
 }
 
-/* Använder funktionerna convert_to_base och convert_to_base_frac 
-för att konvertera ett flyttal till godtycklig bas 'base' */
-char *convert_dec_to_base(double dec_frac, int max_digits, int base){
-	char *base_int = malloc(sizeof(char) * N);
-	char *base_float = malloc(sizeof(char) * N);
-	/* "Plockar ut" heltalet från dec_frac till num, sedan sätts decfrac till 
-	0.##### genom att subtrahera num */
-	int num = dec_frac;
-	printf("convert_dec_to_base: num: %d\n",num);
-	dec_frac = dec_frac - num;
-	printf("convert_dec_to_base: dec_frac: %lf\n",dec_frac);
-	/* Om flyttal, anropas konverteringsfunktion, 
-	annars sätts sträng till nolltecken */
-	if(dec_frac > 0.0000000000){
-		base_float = convert_to_base_frac(dec_frac, max_digits, base);
+//Konverterar heltal med bas 10 till heltal med valfri talbas
+char *convertIntDecToBase(int decimal, int base){
+	char *converted = malloc(sizeof(char) * N);
+	int i = 0, num; // i: index för char-array converted
+	//Om inkommande tal att omvandla är 0, returneras texten '0'
+	if(!decimal){
+		converted[i] = '0';
+		i++;
 	}
 	else{
-		base_float[0] ='\0';
+		/* Siffran är resten av delning med talbasen 'base'
+		på det decimala talet. */
+		for(decimal; decimal != 0; decimal /= base){
+			num = decimal % base;
+			converted[i] = numToChar(num);
+			i++;
+		}
+	}
+	converted[i] = '\0';
+	//Vänder på tecknen i char-arrayen
+	reverseString(converted);
+	return converted;
+}
+
+//Konverterar decimalt flyttal till tal med basen 'base'
+char *convertFracDecToBase(double decimal, int maxDigits, int base){
+	char *converted = malloc(sizeof(char) * N);
+	int i = 0, num; // i: index för char-array converted
+	/* Parameter anses vara i form: 0.##### 
+	Därför sätts strängens början till .# 	*/
+	converted[i] = '.';
+	i++;
+	if(!decimal){
+		converted[i] = '0';
+		i++;
+	}
+	else{
+		for(decimal = decimal * base; decimal != 0.0 && 
+		maxDigits; decimal=decimal * base){
+			num = decimal;
+			decimal = decimal - num ;
+			converted[i] = numToChar(num);
+			i++;
+			maxDigits--;
+		}
+	}
+	converted[i] = '\0';
+	return converted;
+}
+
+char *convertDecToBase(double decimal, int maxDigits, int base){
+	char *baseInt = malloc(sizeof(char) * N);
+	char *baseFrac = malloc(sizeof(char) * N);
+	/* "Plockar ut" heltalet från decimal till num */
+	int num = decimal;
+	decimal = decimal - num;
+	/* Om flyttal, anropas konverteringsfunktion, 
+	annars sätts sträng till nolltecken */
+	if(decimal > 0.0000000000){
+		baseFrac = convertFracDecToBase(decimal, maxDigits, base);
+	}
+	else{
+		baseFrac[0] ='\0';
 	}
 	/* Om inmatat heltal är större än 0, 
 	annars sätts sträng till tecken '0' */
 	if(num){
-		base_int = convert_to_base(num, base);
+		baseInt = convertIntDecToBase(num, base);
 	}
 	else{
-		base_int[0] ='0';
-		base_int[1] ='\0';
+		baseInt[0] ='0';
+		baseInt[1] ='\0';
 	}
 	/* Returnerar sammansatta char-arrayer */
-	return strcat(base_int, base_float);
+	return strcat(baseInt, baseFrac);
 }
 
 
-char *convert_base_to_base(char *num_input, int max_digits, int base_input, int base_output){
-	double dec = convert_base_to_dec(num_input, max_digits, base_input);
+char *convertBaseToBase(char *number, int maxDigits, int baseIn, int baseOut){
+	double convertedDecimal = convertBaseToDec(number, 30, baseIn);
 	char *converted = malloc(sizeof(char) * N);
-	converted = convert_dec_to_base(dec, max_digits, base_output);
+	converted = convertDecToBase(convertedDecimal, maxDigits, baseOut);
 	return converted;
 }
 
 //Konverterar tal med basen 'base' till decimalt flyttal
-double convert_base_to_dec(char *base_frac, int max_digits, int base){
-	char *base_int = malloc(sizeof(char) * N);
-	char *base_float = malloc(sizeof(char) * N);
-	int string_length = strlen(base_frac);
-	//Delar upp strängen till base_int & base_float, vardera sida om eventuellt kommatecken
+double convertBaseToDec(char *number, int maxDigits, int base){
+	char *baseInt = malloc(sizeof(char) * N);
+	char *baseFrac = malloc(sizeof(char) * N);
+	int stringLength = strlen(number);
+	//Delar upp strängen till baseInt & baseFrac, vardera sida om eventuellt kommatecken
 	int i = 0;
-	for(; i < string_length && base_frac[i] != '.'; i++){
-		base_int[i] = base_frac[i];
-		printf("base[%d] = %c \n", i, base_int[i]);
+	for(; i < stringLength && number[i] != '.'; i++){
+		baseInt[i] = number[i];
 	}
-	base_int[i+1] == '\0';
-	int num_int = convert_base_int_to_dec(base_int, base);
-	double num_float = 0.0;
-	if(base_frac[i++] == '.'){
+	baseInt[i+1] == '\0';
+	int convertedInt = convertIntBaseToDec(baseInt, base);
+	double convertedFrac = 0.0;
+	if(number[i++] == '.'){
 		int j = 0;
-		while(base_frac[i] != '\0'){
-			base_float[j] = base_frac[i];
+		while(number[i] != '\0'){
+			baseFrac[j] = number[i];
 			i++; j++;
 		}
-		num_float = convert_base_frac_to_dec(base_float, base);
+		convertedFrac = convertFracBaseToDec(baseFrac, base);
 	}
 	else{
-		base_float[0] = '\0';
+		baseFrac[0] = '\0';
 	}
-	printf("STRINGS: %s   |   %s\n", base_int, base_float);
-	return num_int + num_float;
+	return convertedInt + convertedFrac;
 }
 
 
-int convert_base_int_to_dec(char *base_int, int base){
-	int i = strlen(base_int) - 1, power_of = 0;
+int convertIntBaseToDec(char *number, int base){
+	int i = strlen(number) - 1, powerOf = 0;
 	int converted = 0;
 	for(i; i >= 0; i--){
-		converted += charToNum(base_int[i]) * pow(base, power_of);
-		power_of++;
+		converted += charToNum(number[i]) * pow(base, powerOf);
+		powerOf++;
 	}
 	return converted;
 }
 
-double convert_base_frac_to_dec(char *base_float, int base){
-	int string_length = strlen(base_float), power_of = -1;
+double convertFracBaseToDec(char *number, int base){
+	int stringLength = strlen(number), powerOf = -1;
 	double converted = 0.0;
-	for(int i = 0; i < string_length; i++){
-		converted += charToNum(base_float[i]) * pow(base, power_of);
-		printf("**CONVERTER POW: base: %d pow %d ", base, power_of);
-		printf("**CONVERTER FRAC: NUM: %lf\n", converted);
-		power_of--;
+	for(int i = 0; i < stringLength; i++){
+		converted += charToNum(number[i]) * pow(base, powerOf);
+		powerOf--;
 	}
 	return converted;
 }
--- lib/libconvert.h
+++ lib/libconvert.h
@@ -18,17 +18,17 @@ Johan Kämpe
 //Antal siffror efter kommatecken, för binära flyttal
 #define DECIMALTECKEN 10
 
-void vand(char *str);
+void reverseString(char *str);
 
-char *convert_to_base(int decimal, int base);
-char *convert_to_base_frac(double dec_frac, int max_digits, int base);
-char *convert_dec_to_base(double dec_frac, int max_digits, int base);
+char *convertIntDecToBase(int decimal, int base);
+char *convertFracDecToBase(double decimal, int maxDigits, int base);
+char *convertDecToBase(double decimal, int maxDigits, int base);
 
-double convert_base_to_dec(char *base_frac, int max_digits, int base);
-int convert_base_int_to_dec(char *base_int, int base);
-double convert_base_frac_to_dec(char *base_float, int base);
+double convertBaseToDec(char *number, int maxDigits, int base);
+int convertIntBaseToDec(char *number, int base);
+double convertFracBaseToDec(char *number, int base);
 
-char *convert_base_to_base(char *num_input, int max_digits, int base_input, int base_output);
+char *convertBaseToBase(char *number, int maxDigits, int baseIn, int baseOut);
 
 char numToChar(int num);
 int charToNum(char num);
