a0c34785647762c8f84643e12fe8b8a4ab0a2108
Johan Kämpe <kampe.johan@gmail.com>
2016-12-29 17:00:15 +0100

    Updated to test new function base n to base n

--- conv_tests.c
+++ conv_tests.c
@@ -14,8 +14,8 @@ Code used to test functions in libconvert library
 #include "test/tests.h"
 
 int main(){
-	puts("binary_tests:");
-	binary_tests();//-- OK
+	//puts("binary_tests:");
+	//binary_tests();//-- OK
 	puts("\noctal_tests:");
 	octal_tests();//-- OK
 	puts("\nhexadecimal_tests:");
@@ -28,22 +28,17 @@ int main(){
 	//Manual tests
 	char *converted_number;
 	double  converted_number2;
-	puts("MANUAL CONVERSION");
-		while(1){
-			int base = 2;
-			char num[N];
-			printf("Enter number and base: ");
-			scanf("%s %d", &num, &base);
-			converted_number2 = convert_base_to_dec(num, 10, base);
-			printf("%s(%d) = %.6f(10)\n", num, base, converted_number2);
-	}
+	puts("MANUAL CONVERSION BAS -> BAS");
+
+	//HEX 3A == OCT 72
 	while(1){
-		double dec;
-		int base = 2;
-		printf("Enter decimal number and base: ");
-		scanf("%lf %d", &dec, &base);
-		converted_number = convert_dec_to_base(dec, 10, base);
-		printf("%.10f(10) = %s(%d)\n", dec, converted_number, base);
+		int base = 2, base2 = 3;
+		char num[N];
+		printf("Enter number | base-in | base-out: ");
+		scanf("%s %d %d", &num, &base, &base2);
+		converted_number = convert_base_to_base(num, 10, base, base2);
+		printf("base2: %d\n", base2);
+		printf("%s(%d) = %s(%d)\n", num, base, converted_number, base2);
 	}
 	return 0;
 }
