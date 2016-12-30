27fdcf6831dd56409d30f8daf00113105eb329a3
Johan Kämpe <kampe.johan@gmail.com>
2016-12-30 13:12:55 +0100

    Code that tests binary arithmetic with libconvert

---
+++ binary_arithmetic.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lib/libconvert.h"
+
+//Binary arithmetic using libconvert
+
+int main(){
+	//Binary division
+	int decimal1 = 20014, decimal2 = 4;
+	char *binString1 = convertDecToBase(decimal1, 10, 2);
+	char *binString2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s / ", binString1);
+	printf("%s = ", binString2);
+	printf("%s\n", convertDecToBase(decimal1 / decimal2, 10, 2));
+	//Binary multiplier
+	decimal1 = 127;
+	decimal2 = 51;
+	binString1 = convertDecToBase(decimal1, 10, 2);
+	binString2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s * ", binString1);
+	printf("%s = ", binString2);
+	printf("%s\n", convertDecToBase(decimal1 * decimal2, 10, 2));
+}
