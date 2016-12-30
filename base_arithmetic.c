79b5059cc2314a24765695840c3fafba6e6c2f55
Johan Kämpe <kampe.johan@gmail.com>
2016-12-30 14:17:15 +0100

    Added tests fo hexadecimal arithmetic

--- base_arithmetic.c
+++ base_arithmetic.c
@@ -4,27 +4,38 @@
 //Binary arithmetic using libconvert
 
 int main(){
+	printf("Binary arithmetic tests:\n");
 	//Binary division
 	int decimal1 = 20014, decimal2 = 4;
-	char *binString1 = convertDecToBase(decimal1, 10, 2);
-	char *binString2 = convertDecToBase(decimal2, 10, 2);
-	printf("%s / ", binString1);
-	printf("%s = ", binString2);
+	char *string1 = convertDecToBase(decimal1, 10, 2);
+	char *string2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s / ", string1);
+	printf("%s = ", string2);
 	printf("%s\n", convertDecToBase(decimal1 / decimal2, 10, 2));
-	//Binary multiplier
-	decimal1 = 127;
-	decimal2 = 51;
-	binString1 = convertDecToBase(decimal1, 10, 2);
-	binString2 = convertDecToBase(decimal2, 10, 2);
-	printf("%s * ", binString1);
-	printf("%s = ", binString2);
-	printf("%s\n", convertDecToBase(decimal1 * decimal2, 10, 2));
 	//Binary division, fraction output
 	decimal1 = 10;
 	decimal2 = 3;
-	binString1 = convertDecToBase(decimal1, 10, 2);
-	binString2 = convertDecToBase(decimal2, 10, 2);
-	printf("%s / ", binString1);
-	printf("%s = ", binString2);
+	string1 = convertDecToBase(decimal1, 10, 2);
+	string2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s / ", string1);
+	printf("%s = ", string2);
 	printf("%s\n", convertDecToBase((double)decimal1 / (double)decimal2, 10, 2));
+	//Binary multiplier
+	decimal1 = 127;
+	decimal2 = 51;
+	string1 = convertDecToBase(decimal1, 10, 2);
+	string2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s * ", string1);
+	printf("%s = ", string2);
+	printf("%s\n", convertDecToBase(decimal1 * decimal2, 10, 2));
+	//Hexadecimal division
+	printf("Hexadecimal arithmetic tests:\n");
+	decimal1 = 15236;
+	decimal2 = 2749;
+	string1 = convertDecToBase(decimal1, 10, 16);
+	string2 = convertDecToBase(decimal2, 10, 16);
+	printf("%s / ", string1);
+	printf("%s = ", string2);
+	printf("%s\n", convertDecToBase((double)decimal1 / (double)decimal2, 10, 16));
+	printf("Hexadecimal arithmetic tests:\n");
 }
