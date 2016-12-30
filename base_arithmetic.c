c2df76a8e0c60dd1ff80883e1ea1c9af0616111f
Johan Kämpe <kampe.johan@gmail.com>
2016-12-30 13:21:10 +0100

    Renamed file

--- base_arithmetic.c
+++ base_arithmetic.c
@@ -19,4 +19,12 @@ int main(){
 	printf("%s * ", binString1);
 	printf("%s = ", binString2);
 	printf("%s\n", convertDecToBase(decimal1 * decimal2, 10, 2));
+	//Binary division, fraction output
+	decimal1 = 10;
+	decimal2 = 3;
+	binString1 = convertDecToBase(decimal1, 10, 2);
+	binString2 = convertDecToBase(decimal2, 10, 2);
+	printf("%s / ", binString1);
+	printf("%s = ", binString2);
+	printf("%s\n", convertDecToBase((double)decimal1 / (double)decimal2, 10, 2));
 }
