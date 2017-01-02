192ce634fe06a23f9f6d0e05f1a2b47238d1eaab
Johan Kämpe <kampe.johan@gmail.com>
2017-01-02 15:43:01 +0100

    More comments

--- talbas/conv-to-base.c
+++ talbas/conv-to-base.c
@@ -46,7 +46,7 @@ char get_digit(int digit) {
 }
 
 /*
-	Algorithm for converting decimal to any base
+	Algorithm for converting decimal integer to any base
 		decimal = decimal number to convert.
 		base = the base to convert to (eg 16 for hexadecimal or 2 for binary)
 		
@@ -96,7 +96,6 @@ char *convert_to_base(int decimal, int base) {
 	For each loop decimal is set to decimal divided by base:
 		decimal /= base (same as decimal = decimal / base)
 	*/
-	
    for(decimal; decimal != 0; decimal /= base){
 		/* rest is set to modulus division by base, 
 			this is the digit in the converted number. */
@@ -122,37 +121,91 @@ char *convert_to_base(int decimal, int base) {
    return omvand; //Done!
 }
 
+/*
+	Algorithm for converting decimal float < 1 to any base
+		decimal_frac = decimal number to convert.
+		base = the base to convert to (eg 16 for hexadecimal or 2 for binary)
+		
+		1: Multiply decimal_frac by base
+		2: The integer part of tha value is the first digit in the converted number
+		3: Remove the integer part from decimal_frac
+		3: Repeat until decimal_frac is zero, or until max digits has been reached
+		
+		Example:
+		
+		decimal_frac 0,6875, to Octal (base 8):
+			
+			0,6875 * 8 = 5,5
+			
+			First digit is 5
+			
+			0,5 * 8 = 4,0
+			
+			Second digit is 4
+			
+			decimal_frac is now 0 and conversion is complete
+			Result is: 0,54
+*/
+
+
+/*
+Converts decimal numbers smaller than 1 to any base */
 char *convert_to_base_frac(float dec_frac, int max_digits, int base) {
 	int heltal, oix = 1;
+	/* Set first index in string to '.', so output will be in format ".12345", 
+		hence index oix starts at 1 */
 	omvand_frac[0] = '.';
+   /*	If input to convert is zero, set char  '0' to string and skip conversion, 
+		this skips the conversion */
 	if(dec_frac == 0){
 		omvand_frac[oix] = '0';
-		oix++;
+		oix++; //Need to increase to set Null character
 	}
 	else{
+		/* Conversion alhorithm:
+		The loop runs while decimal_frac is not zero and max_digits is not zero
+		
+		For each loop decimal_frac is set to decimal_frac multiplied by base
+		dec_frac *= base (same as dec_frac = dec_frac * base)
+		
+		Note that the loop starts as dec_frac *= base
+	*/
 		for(dec_frac *= base; dec_frac != 0.0 && max_digits; dec_frac *= base){
+			/*	Heltal gets only the integer part of dec_frac	*/
 			heltal = dec_frac;
+			/*	Remove the integer part from dec_frac, dec_frac is now again < 1	*/
 			dec_frac -= heltal;
+			/* 'omvand_frac' is the global char array used to store
+			converted numbers. Here we set the number in heltal
+			to the index-place in the string that is oix.
+			
+			The function get_digit (in this file) converts the
+			passed integer to a char. So if the passed integer
+			is 7, the function returns the char '7'.
+			'A' if 10, 'B' if 11, etc.
+		*/
 			omvand_frac[oix] = get_digit(heltal);
-			max_digits--;
-			oix++;
+			max_digits--; //Decrease by one, for loop will end if this reaches zero
+			oix++; //increase index for char-array omvand for each loop.
 		}
 	}
 	omvand_frac[oix] = '\0';
-	return omvand_frac;
+	return omvand_frac;  //Done! - Note that we do not reverse the string in this function
 }
 
+/* Uses functions convert_to_base_frac and convert_to_base to convert any decimal number */
 char *convert_to_base_both(double dec_frac, int max_digits, int base){
+	//Separate int part from float
 	int heltal = dec_frac;
-	double frac = dec_frac - heltal;
-	//Globala char arrayer, behöver ej returvärde
+	double frac = dec_frac - heltal; //frac is < 1
+	//As char arrays are global in this file, i dont use the return value from functions
 	convert_to_base(heltal, base);
 	convert_to_base_frac(frac, max_digits, base);
+	//Return omvand_frac added to end of string omvand.
 	return strcat(omvand, omvand_frac);
 }
 
-
-#include "tests.h"
+#include "tests.h" //Tests written by teacher TomKi
 
 int main(void) {
    int decimal = 826;
@@ -168,7 +221,7 @@ int main(void) {
    char *str3 = strdup(convert_to_base(3, 16));
    char *str4 = strdup(convert_to_base_frac(.141592654, 20, 16));
    printf("PI = %s%s(16)\n", str3, str4);
-   //Egen test av sammansatt funktion
+   //Own test to check "convert_to_base_both"
    char *returnString;
    returnString = convert_to_base_both(3.141592654, 20, 16);
    printf("PI = %s(16)\n", returnString);
