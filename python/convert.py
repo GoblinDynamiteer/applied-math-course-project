5dec56012f596fbe86c1b5bc5241e9421f411c81
Johan Kämpe <kampe.johan@gmail.com>
2017-01-03 17:04:38 +0100

    Built convert_to_base_frac, changed returns to strings

--- python/convert.py
+++ python/convert.py
@@ -1,18 +1,40 @@
 # -*- coding: latin-1 -*-
+import math
 
+#Convert integers to any base (up to Z..)
 def convert_to_base(num, base): #definition of functions in python
-	convertedString = []
-	alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	convertedList = []
+	alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" #For digits 
 	while num:
 		rest = num % base;
 		digit = alpha[rest]
-		convertedString.append(digit)
+		convertedList.append(digit)
 		num //= base; # // is integer division
-		#reversedString = convertedString[::-1] #reverses
-	convertedString.reverse()
+	convertedList.reverse()
+	convertedString = ''.join(convertedList)
+	return convertedString
+	
+#Convert floats <1 to any base (up to Z..)
+def convert_to_base_frac(num, base, maxDigits):
+	#num shall be < 0
+	convertedList = ['.']
+	alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ" #For digits 
+	while num > 0 and maxDigits > 0:
+		num *= base
+		integer = math.trunc(num); #truncates the integer
+		digit = alpha[integer]
+		convertedList.append(digit)
+		num -= integer;
+		maxDigits -=1
+	convertedString = ''.join(convertedList)
 	return convertedString
 
-num = int(input("Input number: "))
-base = int(input("Input base: "))
-converted = convert_to_base(num, base)
-print("Konverterat: ",converted)
+#num = float(input("Input number to convert: "))
+#base = int(input("Input base to convert to: "))
+#maxDigits = int(input("Max digits: "))
+#converted = convert_to_base(num, base)
+#converted = convert_to_base_frac(num, base, maxDigits)
+converted = convert_to_base_frac(0.3523256, 16, 10)
+print("0.3523256 Konverterat: ",converted)
+converted = convert_to_base(123456789, 16)
+print("123456789 Konverterat: ",converted)
